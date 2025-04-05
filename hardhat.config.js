e8abdf8e55cdd145d685a636ea84fa48a78aef8b
Aaryan-Dadu <aaryandadu5157@gmail.com>
2025-04-05 16:32:01 +0530

    API key updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,7 @@ module.exports = {
       chainId:1337
     },
     sepolia:{
-      url:"https://eth-sepolia.g.alchemy.com/v2/8M-A8S59VNodqtUAIom94hhtunRlqYHF",
+      url:"https://eth-sepolia.g.alchemy.com/v2/BxzYmU-8xP1gehpFVNVQZpdV9Xf41IOp",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
     }
   },
